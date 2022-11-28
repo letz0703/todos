ee3e6afce7a38bb9faef6e6794e76dc1a27e1929
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-11-28 12:27:36 +0900

    ,,,

--- src/components/delete_task/delete_task.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-//import styles from './delete_task.module.css'
-
-
-export default function DeleteTask({handleDelete}) {
-	return <button onClick={handleDelete}>🗑️</button>
-}
