f14016ddd4c9ed1984d89e1ed8c66ec6019993cc
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-11-29 15:47:47 +0900

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import Header from "./components/header/header"
 import TodoList from "./components/todo_list/todo_list"
 
+const filters = ['all', 'active', 'completed']
+
 function App() {
+  const [filter, setFilter] = useState(filters[0])
   return (
     <div className='App'>
-      <Header />
-      <TodoList />
+      <Header filters={filters} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   )
 }
