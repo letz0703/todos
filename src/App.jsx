06d39c8a84a9044a16ac50223203c04597129b43
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2022-11-29 15:11:00 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react'
 import './App.css'
+import Header from "./components/header/header"
 import TodoList from "./components/todo_list/todo_list"
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <TodoList />
     </div>
   )
